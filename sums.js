be764cb9845eaf9ff5e61e1dc185382d9035faca
realredtext <100484201+realredtext@users.noreply.github.com>
2022-04-18 20:57:30 -0400

    dumb little trilogy from OWOT discord server

---
+++ sums.js
@@ -0,0 +1,5 @@
+let Sums = {
+    intTo: function(max) {return ((max**2)/2) + max/2},
+    squaresTo: function(max) {return (max*(max+1)*(2*max+1))/6},
+    cubesTo: function(max) {return ((max*(max+1))/2)**2}
+}
